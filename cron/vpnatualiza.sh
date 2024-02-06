5f681c7f7dd6c66e9fb76e86bcd3011ff05f8a3b
Guilherme <guilhermecb10@hotmail.com>
2024-02-06 20:54:36 -0300

    feat: feat openvpn

--- cron/vpnatualiza.sh
+++ cron/vpnatualiza.sh
@@ -1,5 +1,5 @@
-docker logs -n150 vpnonline |uniq |grep -i verify |awk  '{print $6,$10}' |grep -v Easy |grep -v ccd > apache/index2.html
+docker logs -n150 openvpn |uniq |grep -i verify |awk  '{print $6,$10}' |grep -v Easy |grep -v ccd >  ../apache/index2.txt
 
-echo 'Hora da consulta:' > apache/index3.html ; date >> apache/index3.html
+echo 'Hora da consulta:' > ../apache/index3.txt ; date >> ../apache/index3.txt
 
-ls  /home/ubuntu/certificados/*.ovpn |wc -l > apache/index4.html
+ls ../certificados/*.ovpn |wc -l > ../apache/index4.txt
